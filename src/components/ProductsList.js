6c5084263060156e3eca856ac9c4cb34388945c5
Nancy Trinh <nancytrinh20@gmail.com>
2020-02-10 12:30:09 -0800

    Fixed ProductsList

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import EditableProduct from './EditableProduct';
 
 const ProductsList = ({ products }) => {
-  const allProducts = products.map(product => {
+  const allProducts = products.map(product => (
     <EditableProduct key={product.id} product={product} />
-  });
+  ));
 
   return (
       <div class="product-listing">
