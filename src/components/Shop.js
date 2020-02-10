965d3949a9c1bb355a291b074864221ce08a4f28
Nancy Trinh <nancytrinh20@gmail.com>
2020-02-10 11:15:57 -0800

    Extract ToggleableProductForm

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,45 +1,23 @@
 import React, { Component } from 'react';
 import Cart from './Cart';
 import ProductsList from './ProductsList';
+import ToggleableProductForm from './ToggleableProductForm';
 
 class Shop extends Component {
   render() {
     return (
-  <div id="app">
-    <header>
-      <h1>The Shop!</h1>
-      <Cart />
-    </header>
+      <div id="app">
+        <header>
+          <h1>The Shop!</h1>
+          <Cart />
+        </header>
 
-    <main>
-      <ProductsList />
-      <div class="add-form visible">
-        <p><a class="button add-product-button">Add A Product</a></p>
-        <h3>Add Product</h3>
-        <form>
-          <div class="input-group">
-            <label for="product-name">Product Name</label>
-            <input type="text" id="product-name" value=""/>
-          </div>
-
-          <div class="input-group">
-            <label for="product-price">Price</label>
-            <input type="text" id="product-price" value=""/>
-          </div>
-
-          <div class="input-group">
-            <label for="product-quantity">Quantity</label>
-            <input type="text" id="product-quantity" value=""/>
-          </div>
-
-          <div class="actions form-actions">
-            <a class="button">Add</a>
-            <a class="button">Cancel</a>
-          </div>
-        </form>
+        <main>
+          <ProductsList />
+          <ToggleableProductForm />
+        </main>
       </div>
-    </main>
-  </div>    );
+    );
   }
 }
 
