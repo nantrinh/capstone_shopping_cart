e604f17aefff95996bf8800dfa1e6ef32708e9c5
Nancy Trinh <nancytrinh20@gmail.com>
2020-02-10 11:02:43 -0800

    Extract cart to component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cart from './Cart';
 
 class Shop extends Component {
   render() {
@@ -6,31 +7,7 @@ class Shop extends Component {
   <div id="app">
     <header>
       <h1>The Shop!</h1>
-      <div class="cart">
-        <h2>Your Cart</h2>
-        <table class="cart-items">
-          <tr>
-            <th>Item</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-          <tr>
-            <td>Amazon Kindle E-reader</td>
-            <td>2</td>
-            <td>$79.99</td>
-          </tr>
-          <tr>
-            <td>Apple 10.5-Inch iPad Pro</td>
-            <td>1</td>
-            <td>$649.99</td>
-          </tr>
-
-          <tr>
-            <td colspan="3" class="total">Total: $729.98</td>
-          </tr>
-        </table>
-        <a class="button checkout">Checkout</a>
-      </div>
+      <Cart />
     </header>
 
     <main>
