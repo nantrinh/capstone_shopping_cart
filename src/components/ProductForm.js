c03410bca33692b1d2371ea38527d072b010f94b
Melissa Manousos <mmanousos@gmail.com>
2020-02-11 11:16:14 -0800

    Moves form state to ToggleableProductForm

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,50 +1,38 @@
 import React, { Component } from 'react';
 
-class ProductForm extends Component {
-  state = {
-    name: '',
-    price: '',
-    quantity: '',
-    edit: false,
-  }
-
-  handleChange = (e) => {
+const ProductForm = (props) => {  
+  function handleChange(e) {
     e.preventDefault();
     const property = e.target.id.split('-')[1];
-    this.setState({
-      [property]: e.target.value
-    })
-    console.log(this.state);
+    props.onChange(property, e.target.value);
   }
 
-  render() {
-    return (
-      <form>
-        <div class="input-group">
-          <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value={this.state.name} 
-            onChange={this.handleChange}/>
-        </div>
-  
-        <div class="input-group">
-          <label for="product-price">Price</label>
-          <input type="text" id="product-price" value={this.state.price}
-            onChange={this.handleChange}/>
-        </div>
-  
-        <div class="input-group">
-          <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value={this.state.quantity}
-            onChange={this.handleChange}/>
-        </div>
-  
-        <div class="actions form-actions">
-          <a class="button">{this.state.edit ? 'Update' : 'Add'}</a>
-          <a class="button">Cancel</a>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form>
+      <div class="input-group">
+        <label for="product-name">Product Name</label>
+        <input type="text" id="product-name" value={props.name} 
+          onChange={handleChange}/>
+      </div>
+
+      <div class="input-group">
+        <label for="product-price">Price</label>
+        <input type="text" id="product-price" value={props.price}
+          onChange={handleChange}/>
+      </div>
+
+      <div class="input-group">
+        <label for="product-quantity">Quantity</label>
+        <input type="text" id="product-quantity" value={props.quantity}
+          onChange={handleChange}/>
+      </div>
+
+      <div class="actions form-actions">
+        <a class="button">{props.edit ? 'Update' : 'Add'}</a>
+        <a class="button">Cancel</a>
+      </div>
+    </form>
+  );
 };
 
 export default ProductForm;
--- src/components/ToggleableProductForm.js
+++ src/components/ToggleableProductForm.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ProductForm from './ProductForm';
 
-const ToggleableProductForm = () => {
-  return (
-    <div class="add-form visible">
-      <p><a class="button add-product-button">Add A Product</a></p>
-      <h3>Add Product</h3>
-      <ProductForm />
-    </div>
-  );
-};
+class ToggleableProductForm extends Component {
+  state = {
+    name: '',
+    price: '',
+    quantity: '',
+  }
+    
+  handleChange = (property, value) => {
+    this.setState({
+      [property]: value
+    })
+    console.log(this.state);
+  }
+      
+  render() {
+    return (
+      <div class="add-form visible">
+        <p><a class="button add-product-button">Add A Product</a></p>
+        <h3>Add Product</h3>
+        <ProductForm 
+          name={this.state.name}
+          price={this.state.price}
+          quantity={this.state.quantity}
+          edit={false}
+          onSubmit={this.props.onSubmit} 
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+}  
 
 export default ToggleableProductForm;
