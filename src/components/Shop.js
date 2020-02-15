73919c3539b367d3984d933a329123df9b642250
Nancy Trinh <nancytrinh20@gmail.com>
2020-02-15 15:42:12 -0800

    Remove unnecessary import

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Cart from "./Cart";
 import ProductsListContainer from "./ProductsListContainer";
 import ToggleableProductForm from "./ToggleableProductForm";
