a958d6c67c5fcc63cf22ededdb991b4083bfa15c
Nancy Trinh <nancytrinh20@gmail.com>
2020-02-14 18:28:01 -0800

    Fix merge conflicts

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,5 @@
-import { uuid } from 'uuidv4'; 
-
+import { uuid } from "uuidv4";
 let uuid1 = uuid();
-
 let products = [
   {
     id: uuid1,
@@ -28,7 +26,6 @@ let products = [
     price: 13.74
   }
 ];
-
 const client = {
   get: url => {
     return new Promise((resolve, reject) => {
@@ -43,19 +40,11 @@ const client = {
       }
     });
   },
-<<<<<<< HEAD
-
-=======
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
   post: (url, data) => {
     return new Promise((resolve, reject) => {
       if (!data.title || !data.quantity || !data.price) {
         reject("Please provide all fields");
       }
-<<<<<<< HEAD
-
-=======
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
       if (url === "/api/products") {
         let product = Object.assign({ id: uuid() }, data);
         products.push(product);
@@ -65,38 +54,19 @@ const client = {
       }
     });
   },
-<<<<<<< HEAD
-
-=======
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
   put: (url, data) => {
     return new Promise((resolve, reject) => {
       if (!data.title && !data.quantity && !data.price) {
         reject("Please provide at least one field");
       }
-<<<<<<< HEAD
-
-=======
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
       if (url.match(/\/api\/products\/.+/)) {
         let id = url.split("/")[3];
         let product = products.find(p => p.id === id);
         let newProduct = Object.assign({}, product, data);
-<<<<<<< HEAD
-
         products = products.map(p => {
           return p.id === id ? newProduct : p;
         });
-<<<<<<< HEAD
-
-=======
-        products = products.map(p => {
-          return p.id === id ? newProduct : p;
-        });
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
-=======
         console.log(products);
->>>>>>> master
         resolve(newProduct);
       } else {
         reject("Invalid URL!");
@@ -109,10 +79,6 @@ const client = {
       if (!products.some(p => p.id === id)) {
         reject("Invalid product");
       }
-<<<<<<< HEAD
-
-=======
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
       if (url.match(/\/api\/products\/.+/)) {
         let product = products.find(p => p.id === id);
         let index = products.indexOf(product);
@@ -124,17 +90,6 @@ const client = {
     });
   }
 };
-<<<<<<< HEAD
-
-/* **USAGE**
-client.get('/api/products')
-      .then((products) => console.log(products));
-
-client.get(`/api/products/${uuid1}`)
-      .then((product) => console.log(product));
-
-
-=======
 /* **USAGE**
 client.get('/api/products')
       .then((products) => console.log(products));
@@ -143,37 +98,19 @@ client.get(`/api/products/${uuid1}`)
       .then((product) => console.log(product));
 ​
 ​
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
 client.post(`/api/products`, {title: 'ABC', quantity: 25, price: 12})
       .then((product) => {
         console.log(product);
       });
-<<<<<<< HEAD
-
-=======
 ​
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
 client.put(`/api/products/${uuid1}`, {title: 'Book Reader'})
       .then((product) => {
         console.log(product);
       });
-<<<<<<< HEAD
-
-=======
 ​
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
 client.delete(`/api/products/${uuid1}`)
       .then(() => {
         
       });
 */
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 export default client;
-=======
-export default client;
->>>>>>> 3e113073041de0443805e2721436020cd58ba957
-=======
-export default client;
->>>>>>> master
